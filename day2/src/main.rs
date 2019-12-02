a14db308c5879e0630b99a6cc23e9950acef5acf
John Hungerford <jhungerford@gmail.com>
2019-12-02 07:15:23 -0700

    Day 2 part 1.

---
+++ day2/src/main.rs
@@ -0,0 +1,49 @@
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn run_program(program: &mut Vec<i32>) {
+    let mut pc = 0;
+
+    while program[pc] != 99 {
+        let source_index_1 = program[pc + 1] as usize;
+        let source_index_2 = program[pc + 2] as usize;
+        let result_index = program[pc + 3] as usize;
+        
+        match program[pc] {
+            1 => { 
+                // 1 means add the numbers at positions pc + 1 and pc + 2, and store them in pc + 3
+                program[result_index] = program[source_index_1] + program[source_index_2]
+            }
+            2 => { 
+                // 2 means multiply the numbers at position pc + 1 and pc + 2, and store them in pc + 3
+                program[result_index] = program[source_index_1] * program[source_index_2]
+            }
+            _ => {
+                panic!("Unknown opcode {} at position {} - {:?}", program[pc], pc, program);
+            }
+        }
+
+        pc += 4
+    };
+}
+
+fn main() -> std::io::Result<()> {
+    let f = File::open("input.txt")?;
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+    line = line.trim().to_string();
+
+    let nums = line.split(",").flat_map(str::parse::<i32>).collect::<Vec<_>>();
+
+    // Part 1: replace position 1 with 12 and position 2 with 2 - what value is left at position 0 after the program halts?
+    let mut part1 = nums.clone();
+    part1[1] = 12;
+    part1[2] = 2;
+    run_program(&mut part1);
+    
+    println!("Part 1: {} - {:?}", part1[0], part1);
+
+    Ok(())
+}
